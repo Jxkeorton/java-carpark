1834c8945f9147507b984816680d650f80258f95
Jake Orton <jakeorton99@gmail.com>
2024-11-29 14:39:06 +0000

    Add try catch to auth class

--- CarPark/src/Authenticate.java
+++ CarPark/src/Authenticate.java
@@ -1,20 +1,26 @@
+import javax.swing.*;
 
 public class Authenticate extends App {
     public void authenticate(String username, String password) {
 
         HandleCSV handleCSV = new HandleCSV();
-        handleCSV.read("credentials");
-
-        String loginUsername = App.credentials[0];
-        String loginPassword = App.credentials[1];
-
-        if (username.equals(loginUsername) && password.equals(loginPassword)) {
-            isLoggedIn = true;
-
-            Gui gui = new Gui();
-            gui.AdminScreen();
-        } else {
-            JOptionPane.showMessageDialog(this, "Invalid username or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+        try {
+            handleCSV.read("credentials");
+    
+            String loginUsername = App.credentials[0];
+            String loginPassword = App.credentials[1];
+    
+            if (username.equals(loginUsername) && password.equals(loginPassword)) {
+                isLoggedIn = true;
+    
+                Gui gui = new Gui();
+                gui.AdminScreen();
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid username or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
+                isLoggedIn = false;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error reading credentials.", "Error", JOptionPane.ERROR_MESSAGE);
             isLoggedIn = false;
         }
     }
