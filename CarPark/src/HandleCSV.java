b84a5786bc4a7c0340a36752cff6c11d713351d0
Jake Orton <jakeorton99@gmail.com>
2024-12-06 15:11:03 +0000

    Edit where attribute declared

--- CarPark/src/HandleCSV.java
+++ CarPark/src/HandleCSV.java
@@ -1,29 +1,31 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class HandleCSV {
-
+    File file = new File("CarPark/VehicleData.csv");
+    File credentialsFile = new File("CarPark/Credentials.csv");
+  
     //Read
     public void read(String fileType) {
         try {
             if (fileType.equals("credentials")) {
                 System.out.println("fetch credentials csv");
-
+ 
                 try {
-                    File credentialsFile = new File("CarPark/Credentials.csv");
+                   
                     Scanner myReader = new Scanner(credentialsFile);
-
+ 
                     String password = "";
                     String username = "";
-
+ 
                     while (myReader.hasNextLine()) {
                         String data = myReader.nextLine();
                         String[] dataArray = data.split(",");
-
+ 
                         username = dataArray[0];
                         password = dataArray[1];
                     }
-
+ 
                     myReader.close();
                     App.credentials = new String[]{username, password};
                 } catch (FileNotFoundException e) {
@@ -31,29 +33,29 @@ public class HandleCSV {
                     System.out.println("Looking in: " + new File("../Credentials.csv").getAbsolutePath());
                     App.credentials = new String[]{"", ""};
                 }
-
+ 
             } else if (fileType.equals("carpark")) {
                 System.out.println("fetch carpark csv");
-
+ 
                 try {
                     File vehiclesFile = new File("CarPark/VehicleData.csv");
                     Scanner myReader = new Scanner(vehiclesFile);
                     StringBuilder carparkData = new StringBuilder();
-                    
+                   
                     if (myReader.hasNextLine()) {
                         myReader.nextLine();
                     }
-
+ 
                     while (myReader.hasNextLine()) {
                         String data = myReader.nextLine();
                         carparkData.append(data).append("\n");
                     }
-                    
+                   
                     myReader.close();
-
+ 
                     String data = carparkData.toString().trim();
                     App.carparkData = data.isEmpty() ? new ArrayList<String>() : new ArrayList<>(Arrays.asList(data.split("\n")));
-
+ 
                 } catch (FileNotFoundException e) {
                     System.out.println("Error: Could not find Carpark data file");
                     System.out.println("Looking in: " + new File("CarPark/VehicleData.csv").getAbsolutePath());
@@ -62,7 +64,7 @@ public class HandleCSV {
             } else {
                 App.carparkData = new ArrayList<String>();
             }
-
+ 
         } catch (Exception e) {
             e.printStackTrace();
             App.carparkData = new ArrayList<String>();
@@ -70,7 +72,7 @@ public class HandleCSV {
     }
     //Update
     public void update(ArrayList<String> data) throws IOException {
-        File file = new File("CarPark/VehicleData.csv");
+ 
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         writer.write("VRN,EntryDate,EntryTime,ExitDate,ExitTime,Payment\n");
         for (String row : data) {
